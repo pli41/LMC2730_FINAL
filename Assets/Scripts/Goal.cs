bf6fa52dfaee91843051031c25495b53bb916429
Peilin Li <lipeilin@gatech.edu>
2014-11-06 17:21:26 -0500

    Goal feature added

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			if(player.getReadyForGoal()){
+				Debug.Log("Victory");
+			}
+		}
+
+	}
+}
